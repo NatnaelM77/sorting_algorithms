379c53957d8e1958eb6ec760b503f68c283117fb
Natnael <natnaelmehari77@gmail.com>
2022-01-24 12:34:56 +0300

    fix betty linter errors

--- sort.h
+++ sort.h
@@ -11,28 +11,20 @@
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
-typedef struct listint_s {
+typedef struct listint_s
+{
     const int n;
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
 
 void print_array(const int *, size_t);
-
 void print_list(const listint_t *);
-
-void swapInt(int *, int *);
-
 void bubble_sort(int *, size_t);
-
 void insertion_sort_list(listint_t **);
-
 void selection_sort(int *, size_t);
-
 void quick_sort(int *, size_t);
-
 int partition(int *, int, int);
-
 void qs(int *, int, int);
 
 #endif /* SORTING_ALGORITHMS_SORT_H */
